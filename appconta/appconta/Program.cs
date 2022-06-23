a770986d719f362199bc9f4606a6ffcf773ea8d8
esoft27 - Leonardo Martins de Oliveira <ex150339@g.unicamp.br>
2022-06-22 22:51:48 -0300

    Exemplos de aplicações usando docker

---
+++ appconta/appconta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace appconta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contador = 0;
+            var maximo = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+
+            while(maximo == -1 || contador < maximo)
+            {
+                contador++;
+                Console.WriteLine($"Contando... : {contador}");
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
